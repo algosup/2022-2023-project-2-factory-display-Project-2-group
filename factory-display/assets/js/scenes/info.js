e5ba19d08c509119de574624ede31a97e04f5068
RemyCHARLES <100137905+RemyCHARLES@users.noreply.github.com>
2022-11-19 17:24:06 +0100

    Update FirstView Scene
    Add a next button

--- factory-display/assets/js/scenes/info.js
+++ factory-display/assets/js/scenes/info.js
@@ -4,22 +4,33 @@ const landBtn = document.querySelector('#landBtn');
 const portrait = document.querySelector('#portrait');
 const landscape = document.querySelector('#landscape');
 
+const nextBtn = document.querySelector('#nextBtn');
+
 //! console.log(portBtn);
 //! console.log(landBtn);
 //! console.log(portrait);
 //! console.log(landscape);
 
+//* Function to select portrait orientation
 function selecPort (){
   portrait.className='active-orientation';
   landscape.className='inactive-orientation';
   //! console.log('portrait');
 };
 
+//* Function to select landscape orientation
 function selecLand (){
   portrait.className='inactive-orientation';
   landscape.className='active-orientation';
   //! console.log('landscape');
 };
 
+//* Function to go to the second view
+function nextScene (){
+  console.log(loadSecondView());
+};
+
 portBtn.addEventListener('click', selecPort)
 landBtn.addEventListener('click', selecLand) 
+
+nextBtn.addEventListener('click', nextScene)
