c22dc4476f6deb75193c841574bffecd1815dfbe
Nicolas MIDA <nicolas.mida@algosup.com>
2022-11-29 14:23:35 +0100

    Navabar updated to all pages
    Co-Authored-By: audreytllz <114394252+audreytllz@users.noreply.github.com>

--- factory-display/assets/js/section/header.js
+++ factory-display/assets/js/section/header.js
@@ -1,36 +1,36 @@
 function navbarToggle() {
-  const hamburgerToggler = document.querySelector(".hamburger");
-  const navLinksContainer = document.querySelector(".navlinks-container");
+    const hamburgerToggler = document.querySelector(".hamburger");
+    const navLinksContainer = document.querySelector(".navlinks-container");
 
-  const toggleNav = () => {
-    hamburgerToggler.classList.toggle("open");
+    const toggleNav = () => {
+        hamburgerToggler.classList.toggle("open");
 
-    const ariaToggle =
-      hamburgerToggler.getAttribute("aria-expanded") === "true"
-        ? "false"
-        : "true";
-    hamburgerToggler.setAttribute("aria-expanded", ariaToggle);
+        const ariaToggle =
+            hamburgerToggler.getAttribute("aria-expanded") === "true"
+                ? "false"
+                : "true";
+        hamburgerToggler.setAttribute("aria-expanded", ariaToggle);
 
-    navLinksContainer.classList.toggle("open");
-  };
-  hamburgerToggler.addEventListener("click", toggleNav);
+        navLinksContainer.classList.toggle("open");
+    };
+    hamburgerToggler.addEventListener("click", toggleNav);
 
-  new ResizeObserver((entries) => {
-    if (entries[0].contentRect.width <= 900) {
-      navLinksContainer.style.transition = "transform 0.3s ease-out";
-    } else {
-      navLinksContainer.style.transition = "none";
-    }
-  }).observe(document.body);
+    new ResizeObserver((entries) => {
+        if (entries[0].contentRect.width <= 900) {
+            navLinksContainer.style.transition = "transform 0.3s ease-out";
+        } else {
+            navLinksContainer.style.transition = "none";
+        }
+    }).observe(document.body);
 }
 
 function displayHeader(status) {
-  // Status are : admin, user, guest
-  headerCode = document.querySelector("header");
-  switch (status) {
-    case "admin":
-      // Display admin header
-      headerCode.innerHTML = `
+    // Status are : admin, user, guest
+    headerCode = document.querySelector("header");
+    switch (status) {
+        case "admin":
+            // Display admin header
+            headerCode.innerHTML = `
         <nav>
         <a href="#" class="nav-icon" aria-label="visit homepage" aria-current="page">
             <img src="/factory-display/assets/images/jacobi-icon.png" alt="icon jacobi">
@@ -49,7 +49,7 @@ function displayHeader(status) {
                 </div>
                 <div class="dropdown">
                     <div class="main-content">
-                        <a id="container" href="/factory-display/scenes/index.html">Scènes <i class="fa fa-caret-down"></i></a>
+                        <a id="container" href="/factory-display/scenes/index.html">Scènes </a>
                     </div>
                     <article class="dropdown-content">
                         <a id="" href="/factory-display/scenes/create.html">Nouvelle scène</a>
@@ -59,7 +59,7 @@ function displayHeader(status) {
                 </div>
                 <div class="dropdown">
                     <div class="main-content">
-                        <a id="container" href="/factory-display/settings/index.html">Mon Espace <i class="fa fa-caret-down"></i></a>
+                        <a id="container" href="/factory-display/settings/index.html">Mon Espace</a>
                     </div>
                     <article class="dropdown-content">
                         <a id="" href="/factory-display/settings/parametre.html">Paramètres</a>
@@ -70,10 +70,10 @@ function displayHeader(status) {
         </div>
       </nav>
       `;
-      break;
-    case "user":
-      // Display user header
-      headerCode.innerHTML = `
+            break;
+        case "user":
+            // Display user header
+            headerCode.innerHTML = `
         <nav>
         <a href="#" class="nav-icon" aria-label="visit homepage" aria-current="page">
             <img src="/factory-display/assets/images/jacobi-icon.png" alt="icon jacobi">
@@ -92,7 +92,7 @@ function displayHeader(status) {
                 </div>
                 <div class="dropdown">
                     <div class="main-content">
-                        <a id="container" href="/factory-display/scenes/index.html">Scènes<i class="fa fa-caret-down"></i></a>
+                        <a id="container" href="/factory-display/scenes/index.html">Scènes</a>
                     </div>
                     <article class="dropdown-content">
                         <a id="" href="/factory-display/scenes/view.html">Voir les scènes</a>
@@ -100,7 +100,7 @@ function displayHeader(status) {
                 </div>
                 <div class="dropdown">
                     <div class="main-content">
-                        <a id="container" href="/factory-display/settings/index.html">Mon Espace <i class="fa fa-caret-down"></i></a>
+                        <a id="container" href="/factory-display/settings/index.html">Mon Espace </a>
                     </div>
                     <article class="dropdown-content">
                         <a id="" href="/factory-display/settings/parametre.html">Paramètres</a>
@@ -111,10 +111,10 @@ function displayHeader(status) {
         </div>
       </nav>
       `;
-      break;
-    case "guest":
-      // Display guest header
-      headerCode.innerHTML = `
+            break;
+        case "guest":
+            // Display guest header
+            headerCode.innerHTML = `
         <nav>
         <a href="#" class="nav-icon" aria-label="visit homepage" aria-current="page">
             <img src="/factory-display/assets/images/jacobi-icon.png" alt="icon jacobi">
@@ -129,27 +129,13 @@ function displayHeader(status) {
             </button>
             <div class="navlinks-container">
                 <div class="main-content">
-                    <a id="container" href="/factory-display/index.html" aria-current="page">Accueil</a>
+                    <a class="container" href="/factory-display/index.html" aria-current="page">Accueil</a>
                 </div>
-                <div class="dropdown">
-                    <div class="main-content">
-                        <a id="container" href="/factory-display/scenes/index.html">Scènes <i class="fa fa-caret-down"></i></a>
-                    </div>
-                    <article class="dropdown-content">
-                        <a id="" href="/factory-display/scenes/create.html">Nouvelle scène</a>
-                        <a id="" href="/factory-display/scenes/manage.html">Gérer mes scènes</a>
-                        <a id="" href="/factory-display/scenes/view.html">Voir mes scènes</a>
-                    </article>
+                <div class="main-content">
+                    <a class="container" href="/factory-display/scenes/index.html">Scènes </a>
                 </div>
-                <div class="dropdown">
-                    <div class="main-content">
-                        <a id="container" href="/factory-display/settings/index.html">Mon Espace <i class="fa fa-caret-down"></i></a>
-                    </div>
-                    <article class="dropdown-content">
-                        <a id="" href="/factory-display/settings/parametre.html">Paramètres</a>
-                        <a id="" href="/factory-display/settings/compte.html">Mon compte</a>
-                        <a id="" href="/factory-display/settings/account.html">Connexion</a>
-                    </article>
+                <div class="main-content">
+                    <a class="container" href="/factory-display/settings/index.html">Mon Espace </a>    
                 </div>
             </div>
         </div>
@@ -159,17 +145,18 @@ function displayHeader(status) {
                 <img src="/factory-display/assets/images/user-icon.svg" alt="user-icon">
             </a>
             <div class="sign-btns">
-                <button type="button"> <a href="/factory-display/settings/account.html" class="link-btn"> Connexion</a></button>
+                <button type="button"> <a href="/factory-display/settings/account.html" class="link-btn"> Connexion </a></button>
                 <button type="button"><a href="/factory-display/settings/inscription.html" class="link-btn"> Inscription </a></button>            
             </div>
         </div>
       </nav>
       `;
-      break;
-    default:
-      // Display guest header
-      break;
-  }
+            break;
+        default:
+            // Display guest header
+            break;
+    }
 
-  navbarToggle();
+    navbarToggle();
+
 }
