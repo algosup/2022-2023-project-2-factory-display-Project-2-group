92d89bd7fa4ed6e44beea677116f1db818191223
Nicolas MIDA <nicolas.mida@algosup.com>
2022-12-08 12:24:40 +0100

    testing

--- factory-display/assets/php/login/config_db.php
+++ factory-display/assets/php/login/config_db.php
@@ -1,17 +1,18 @@
 <?php
+
+$db_host = 'jacobi-dbserver-grp2.database.windows.net';
+$db_user = 'sqljacobi';
+$db_password = 'pQgxk#6B3tNHDjG4';
+$db_db = 'JacobiDatabase';
+$db_port = 1433;
+
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:jacobi-dbserver-grp2.database.windows.net,1433; Database = JacobiDatabase", "sqljacobi", "pQgxk#6B3tNHDjG4");
+    $conn = new PDO("sqlsrv:server = tcp:$db_host,$db_port; Database = $db_db", "db_user", "$db_password");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
     print("Error connecting to SQL Server.");
     die(print_r($e));
-} 
-
-if($conn){
-    echo "Connection established.<br />";
-}else{
-    echo "Connection could not be established.<br />";
 }
 ?>
