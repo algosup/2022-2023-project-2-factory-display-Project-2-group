4884d9512437dec44c0b14e0f3b379d942ed378c
Nicolas MIDA <nicolas.mida@algosup.com>
2022-12-15 23:34:12 +0100

    Update index.php

--- factory-display/assets/admins/index.php
+++ factory-display/assets/admins/index.php
@@ -1,5 +1,5 @@
 <?php
-@include 'factory-display/assets/php/login/config_db.php';
+@include '../php/login/config_db.php';
 
 session_start();
 
@@ -29,7 +29,72 @@ if (isset($_SESSION['logged_in'])) {
 ?>
 
 <?php
-if ($ok) {
-    echo "ok";
-}
-?>
+if ($ok) { ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="/factory-display/assets/img/icons/jacobi-icon.png">
+    <title>Administration</title>
+
+    <script src="/factory-display/assets/headers/header.js"></script>
+
+
+    <link rel="stylesheet" href="/factory-display/assets/css/section/header.css">
+    <link rel="stylesheet" href="/factory-display/assets/css/libs/bootstrap.css">
+    <link rel="stylesheet" href="/factory-display/assets/css/libs/font-awesome.css">
+    <link rel="stylesheet" href="/factory-display/assets/css/settings/homepage.css">
+    <link rel="stylesheet" href="/factory-display/assets/css/admin/main.css">
+
+    <link href='https://fonts.googleapis.com/css?family=Noto Sans' rel='stylesheet'>
+</head>
+
+<body>
+    <div class="main-content-admin">
+        <div class="admin-header">
+            <h1>Administration</h1>
+        </div>
+
+        <div class="admin-table-user"> <!-- make a table  with all the users from the database -->
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">Nom</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Role</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+    // we fetch the result
+    while ($row = $result->fetch(PDO::FETCH_ASSOC)) { ?>
+                    <tr>
+                        <td>
+                            <?php echo $row['name']; ?>
+                        </td>
+                        <td>
+                            <?php echo $row['email']; ?>
+                        </td>
+                        <td>
+                            <?php echo $row['role']; ?>
+                        </td>
+                        <td>
+                            <form action="process.php" method="post">
+                                <input type="submit" id="<?php echo $row['id']; ?>" name="delete" value="Supprimer"
+                                    class="btn btn-danger">
+                                <input type="submit" id="<?php echo $row['id']; ?>" name="edit" value="Modifier"
+                                    class="btn btn-primary">
+                        </td>
+                    </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</body>
+
+</html>
+<?php } ?>
